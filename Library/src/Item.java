51d328ee42d58e5dae2fc23a5c9d9eba3756de21
Adam Nguyen <dopaimail@gmail.com>
2019-11-04 16:46:38 -0500

    added getters and setters

--- Library/src/Item.java
+++ Library/src/Item.java
@@ -10,13 +10,76 @@ public abstract class Item {
 	protected int maxTime;
 	protected int checkoutTime;
 	protected boolean isNew;
+	protected int numCopies;
+	protected String type;
 	
 	public Item()
 	{
 		
 	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public int getYearPublished() {
+		return yearPublished;
+	}
+	public void setYearPublished(int yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+	public double getRetail() {
+		return retail;
+	}
+	public void setRetail(double retail) {
+		this.retail = retail;
+	}
+	public int getMaxTime() {
+		return maxTime;
+	}
+	public void setMaxTime(int maxTime) {
+		this.maxTime = maxTime;
+	}
+	public int getCheckoutTime() {
+		return checkoutTime;
+	}
+	public void setCheckoutTime(int checkoutTime) {
+		this.checkoutTime = checkoutTime;
+	}
+	public boolean isNew() {
+		return isNew;
+	}
+	public void setNew(boolean isNew) {
+		this.isNew = isNew;
+	}
 	public Item(String creator,String itemName,String description,double rating,
-			String genre,int yearPublished,double retail,int maxTime,int checkoutTime,boolean isNew)
+			String genre,int yearPublished,double retail,int maxTime,int checkoutTime,boolean isNew,
+			int numCopies,String type)
 	{
 		this.creator=creator;
 		this.itemName=itemName;
@@ -28,6 +91,20 @@ public abstract class Item {
 		this.maxTime=maxTime;
 		this.checkoutTime=checkoutTime;
 		this.isNew=isNew;
+		this.numCopies=numCopies;
+		this.type=type;
+	}
+	public int getNumCopies() {
+		return numCopies;
+	}
+	public void setNumCopies(int numCopies) {
+		this.numCopies = numCopies;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
 	}
 	public String toString()
 	{
