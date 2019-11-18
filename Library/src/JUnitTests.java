8dc5f8ac19bf81da701be850e5ab8ef2b033c6c8
JoshMolina <joshua.molina98@gmail.com>
2019-11-17 21:02:15 -0500

    merging

--- Library/src/JUnitTests.java
+++ Library/src/JUnitTests.java
@@ -80,6 +80,9 @@ class JUnitTests {
 	}*/
 	
 	@Test
+	/**
+	 * Login to JD@email.com and checksout The Cat in The Hat, checks if copies of The Cat in The Hat went down by 1
+	 */
 	void checkOutItemPassTest() {
 		SystemShell shell = SystemShell.launchSystem();
 		shell.loginUser("JD@email.com", "123");
@@ -90,13 +93,45 @@ class JUnitTests {
 		assertEquals(copies,copiesAfter+1);
 	}
 	
-	 @Test void checkOutItemFailTest() { 
-		 SystemShell shell =
-		 SystemShell.launchSystem(); shell.loginUser("bob@email.com", "123"); 
-		 int id=1; 
-		 int copies=shell.jsonItemList.get(id-1).getNumCopies();
-		 shell.checkoutItem(id); 
-		 int copiesAfter=shell.jsonItemList.get(id-1).getNumCopies();
-		 assertEquals(copies,copiesAfter); 
-	  }
+	@Test 
+	/**
+	 * Cant checkout because he has fines
+	 */
+	void checkOutItemFailTest() { 
+		SystemShell shell = SystemShell.launchSystem(); 
+		shell.loginUser("bob@email.com", "123"); 
+		int id=1; 
+		int copies=shell.jsonItemList.get(id-1).getNumCopies();
+		shell.checkoutItem(id); 
+		int copiesAfter=shell.jsonItemList.get(id-1).getNumCopies();
+		assertEquals(copies,copiesAfter);
+	}
+	@Test
+	/**
+	 * Adds a fine of $5.0 to JD@email.com
+	 */
+	void addFinePassTest() {
+		SystemShell shell = SystemShell.launchSystem();
+		shell.loginUser("admin@email.com", "123");
+		int id=0;
+		double fineAmount=5.0;
+		double fines=shell.jsonUserList.get(id).getBalance();
+		shell.addFine(fineAmount, "JD@email.com");
+		double finesAfter=shell.jsonUserList.get(id).getBalance();
+		assertEquals(fines+fineAmount,finesAfter);
+	}
+	@Test
+	/**
+	 * Can't add fine because user logged in isn't an admin
+	 */
+	void addFineFailTest() {
+		SystemShell shell = SystemShell.launchSystem();
+		shell.loginUser("bob@email.com", "123");
+		int id=0;
+		double fineAmount=5.0;
+		double fines=shell.jsonUserList.get(id).getBalance();
+		shell.addFine(fineAmount, "JD@email.com");
+		double finesAfter=shell.jsonUserList.get(id).getBalance();
+		assertEquals(fines,finesAfter);
+	}
 }
